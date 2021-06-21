d4b42da324e628e9c5800725278fa7d272d4bd9e
Motibidu <jack981109@gmail.com>
2021-06-21 17:12:12 +0900

    initial commit

---
+++ sftwrEng/src/sftwrEng/B.java
@@ -0,0 +1,10 @@
+package sftwrEng;
+
+public class B {
+
+	public static void main(String[] args) {
+		System.out.println("Hello B");
+
+	}
+
+}
---
+++ sftwrEng/src/sftwrEng/C.java
@@ -0,0 +1,10 @@
+package sftwrEng;
+
+public class C {
+
+	public static void main(String[] args) {
+		System.out.println("Hello C");
+
+	}
+
+}
